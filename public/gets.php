dc51bced76429b1d4e391924f743c2914fa66203
山﨑由翔 <kd1352135@st.kobedenshi.ac.jp>
2024-05-09 13:56:33 +0900

    入力済

---
+++ public/gets.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GETメソッドのデータ</title>
+</head>
+
+<body>
+    <h4><!-- ここに"出席番号 名前"を入れてください --></h4>
+    GETメソッドで送られてきた複数のデータを受け取る。<br>
+        <?php
+        echo "出席番号:" . $_GET['course'] . $_GET['class'] . $_GET['number'] . "、氏名:" . $_GET['name'];
+
+        //追加
+        echo '<br>';
+        var_dump($_GET);
+        ?>
+</body>
+
+</html>
